b3cab8d72d3171626877f3c53ae898350213b1db
Svyatoslav Klyuchev <svyatoslav.klyuchev@gmail.com>
2021-09-22 00:33:59 +0300

    upd

--- NBControllerClient.js
+++ NBControllerClient.js
@@ -1,4 +1,4 @@
-import BleManager from "react-native-ble-manager";
+import BleManager, {connect} from "react-native-ble-manager";
 import {stringToBytes, bytesToString} from "convert-string";
 import { NativeModules, NativeEventEmitter } from "react-native";
 
@@ -8,127 +8,166 @@ class NBControllerClient {
 
     static shared = new NBControllerClient();
 
+    bleManager = BleManager
     bleManagerEmitter = new NativeEventEmitter(BleManagerModule);
     neededService = '0000ffe0-0000-1000-8000-00805f9b34fb';
     neededChar = '0000ffe1-0000-1000-8000-00805f9b34fb';
 
+    scanCheckingPeriod = 500
+    connectionAttempts = 5
+
+    connected = false
 
     constructor() {
-        this.bleManagerEmitter.addListener('BleManagerDisconnectPeripheral', this.onDisconnect.bind(this))
+        this.disconnectSubscription = this.bleManagerEmitter.add('BleManagerDisconnectPeripheral', this.onDisconnect.bind(this))
     }
 
     init = () => {
-        BleManager.start()
+        this.bleManager.start()
             .then(this.onStart.bind(this))
             .catch(this.onStartError.bind(this))
     }
 
     onDisconnect = (deviceId) => {
+        this.connected = false
         console.log("HERE onDisconnect", deviceId)
     }
+    onStart = () => console.log('BleManager started')
+    onStartError = error => console.log(`BleManager start error: ${error.message}`)
 
-    onStart = () => {}
-    onStartError = error => {}
-
-    findDevice = (postmachineName, timeout = 3000, checkPeriod = 500) => {
+    findDevice = (deviceName, searchTimeout = 5000, log = msg => {}) => {
         return new Promise((resolve, reject) => {
-            BleManager.scan([], timeout / 1000, false)
+            this.bleManager.scan([], searchTimeout / 1000, false)
                 .then(() => {
-                    let intervalId = null;
+                    let intervalId = null
                     let timeoutId = setTimeout(() => {
-                        clearInterval(interval);
-                        reject(new Error('device not found'));
-                    }, timeout);
+                        clearInterval(intervalId)
+                        this.cancelScan()
+                        reject(new Error('device not found'))
+                    }, searchTimeout)
 
                     intervalId = setInterval(async () => {
                         try {
                             const discoveredDevices = await BleManager.getDiscoveredPeripherals();
                             const bondedDevices = await BleManager.getBondedPeripherals()
                             const connectedDevices = await BleManager.getConnectedPeripherals();
-                            const devices = [ ...discoveredDevices, ...bondedDevices, ...connectedDevices];
-                            const device = devices.find(item => item.name === postmachineName);
+                            const devices = [...discoveredDevices, ...bondedDevices, ...connectedDevices]
+                            const device = devices.find(item => item.name === deviceName);
                             if (device) {
-                                clearInterval(intervalId)
-                                clearTimeout(timeoutId)
-                                await this.cancelScan()
-                                resolve(device)
+                                clearInterval(intervalId);
+                                clearTimeout(timeoutId);
+                                resolve(device);
                             }
                         } catch (error) {
-                            clearInterval(intervalId);
-                            clearTimeout(timeoutId);
-                            await this.cancelScan()
-                            reject(error);
+                            clearInterval(intervalId)
+                            clearTimeout(timeoutId)
+                            reject(error)
                         }
-                    }, checkPeriod);
+                    }, this.scanCheckingPeriod)
                 })
                 .catch(reject)
         })
     }
 
-    cancelScan = () => BleManager.stopScan()
+    cancelScan = () => this.bleManager.stopScan()
 
-    send = (device, command, responseTime = 500, short = false) => {
+    connect = async (device, log = (error, msg) => {}) => {
+        let error = null
+        for (let i = 0; i < this.connectionAttempts; i++) {
+            try {
+                log(null, `connection attempt ${i + 1}`)
+                await this.bleManager.connect(device.id)
+                log(null, `retrieve attempt ${i + 1}`)
+                await this.bleManager.retrieveServices(device.id);
+                error = null
+                break
+            } catch (err) {
+                log(err)
+                error = err
+            }
+        }
+        if (error) {
+            throw error
+        } else {
+            this.connected = true
+        }
+    }
+
+    send = (deviceName, command, responseTimeout = 500, short = true) => {
         return new Promise(async (resolve, reject) => {
-            let listener = null;
-            let timeoutId = null;
-            let response = null;
-            listener = async ({value}) => {
+            await this.bleManager.startNotification(device.id, this.neededService, this.neededChar);
+            let response = null
+            let timeoutId = null
+            let subscription = null
+
+            const listener = async ({value}) => {
                 const data = bytesToString(value)
                 if (short) {
-                    this.bleManagerEmitter.removeListener("BleManagerDidUpdateValueForCharacteristic", listener)
-                    clearTimeout(timeoutId)
+                    if (subscription) subscription.remove()
+                    if (timeoutId) clearTimeout(timeoutId)
                     resolve(data)
                 } else {
-                    response = response ? response += data : data
+                    response = response ? response + `${data}` : `${data}`
                 }
             }
 
-            this.bleManagerEmitter.addListener("BleManagerDidUpdateValueForCharacteristic", listener)
-            try {
-                await BleManager.writeWithoutResponse(
-                    device.id,
-                    this.neededService,
-                    this.neededChar,
-                    stringToBytes(`${command}\r\n`),
-                    10
-                );
+            subscription = this.bleManagerEmitter.addListener('BleManagerDidUpdateValueForCharacteristic', listener)
 
+            try {
+                await this.bleManager
+                    .writeWithoutResponse(
+                        deviceName,
+                        this.neededService,
+                        this.neededChar,
+                        stringToBytes(`${command}\r\n`),
+                        10
+                    )
                 timeoutId = setTimeout(async () => {
-                    this.bleManagerEmitter.removeListener("BleManagerDidUpdateValueForCharacteristic", listener)
+                    subscription.remove()
                     if (short) {
-                        reject("response timeout")
+                        reject(new Error('response timeout'))
                     } else {
-                        if (response === null) {
-                            reject("response timout");
-                        } else {
-                            resolve(response)
-                        }
+                        resolve(response)
                     }
-                }, responseTime)
+                }, responseTimeout)
             } catch (error) {
-                this.bleManagerEmitter.removeListener("BleManagerDidUpdateValueForCharacteristic", listener)
-                reject(error)
+                return reject(error)
             }
-        });
+        })
     }
 
-    disconnect = id => BleManager.disconnect(id)
+    disconnect = deviceName => this.bleManager.disconnect(deviceName)
 
-    connect = async (deviceId, attemptsCount = 5) => {
-        let connected = false
-        for (let i = 0; i < attemptsCount; i++) {
-            try {
-                await BleManager.connect(deviceId)
-                connected = true
-                break
-            } catch (error) {
-                console.log({ error })
-            }
-        }
-        if (!connected) {
-            throw new Error('not connected')
-        }
+    findAllPostmachinesNearby = (searchTimeout = 5000, log = (error, msg, data) => {}) => {
+        return new Promise((resolve, reject) => {
+            this.bleManager.scan([this.neededService], searchTimeout, false)
+                .then(() => {
+                    let intervalId = null
+                    let devicesList = []
+                    let timeoutId = setTimeout(() => {
+                        if (intervalId) clearInterval(intervalId)
+                        resolve(devicesList)
+                    }, searchTimeout)
+
+                    intervalId = setInterval(async () => {
+                        try {
+                            const discoveredDevices = await BleManager.getDiscoveredPeripherals();
+                            const bondedDevices = await BleManager.getBondedPeripherals()
+                            const connectedDevices = await BleManager.getConnectedPeripherals();
+                            const devices = [...discoveredDevices, ...bondedDevices, ...connectedDevices]
+                            devicesList = devices
+                            log(null, "devices", devices)
+                        } catch (error) {
+                            clearInterval(intervalId)
+                            clearTimeout(intervalId)
+                            reject(error)
+                        }
+                    }, this.scanCheckingPeriod)
+                })
+                .catch(reject)
+        })
     }
+
 }
 
 export default NBControllerClient
